3ea19ea64e3d5dc3e53e59ef2500cf13bac9bdb8
Till <Till-Bernas@web.de>
2023-01-31 17:36:59 +0100

    send json in body of Error

--- src/main/java/com/mythostrike/controller/RestExceptionHandler.java
+++ src/main/java/com/mythostrike/controller/RestExceptionHandler.java
@@ -1,9 +1,15 @@
 package com.mythostrike.controller;
 
-/*@ControllerAdvice
+import com.mythostrike.controller.message.ErrorMessage;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+@ControllerAdvice
 public class RestExceptionHandler {
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<ErrorMessage> handleResponseStatusException(ResponseStatusException exception) {
         return new ResponseEntity<>(new ErrorMessage(exception.getReason()), exception.getStatusCode());
     }
-}*/
+}
