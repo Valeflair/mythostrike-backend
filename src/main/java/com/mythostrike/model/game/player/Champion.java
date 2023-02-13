cdbae9a988c312e2a7c618cf689c26bc551d831f
Till <Till-Bernas@web.de>
2023-02-13 19:46:56 +0100

    fixed champion active skill id

--- src/main/java/com/mythostrike/model/game/player/Champion.java
+++ src/main/java/com/mythostrike/model/game/player/Champion.java
@@ -15,25 +15,15 @@ public class Champion {
     private final List<PassiveSkill> passiveSkills;
     private final List<ActiveSkill> activeSkills;
 
-    public Champion(int id, String name, int maxHp, List<PassiveSkill> passiveSkills,
-                    List<ActiveSkill> activeSkills) {
-        this.id = id;
-        this.name = name;
-        this.maxHp = maxHp;
-        this.passiveSkills = List.copyOf(passiveSkills);
-        this.activeSkills = List.copyOf(activeSkills);
-
-        for (int i = 0; i < activeSkills.size(); i++) {
-            activeSkills.get(i).initialize(i);
-        }
-    }
-
     public Champion(int id, ChampionData data) {
         this.id = id;
         this.name = data.getName();
         this.maxHp = data.getMaxHp();
         this.passiveSkills = List.copyOf(data.getPassiveSkills());
         this.activeSkills = List.copyOf(data.getActiveSkills());
-    }
 
+        for (int i = 0; i < activeSkills.size(); i++) {
+            activeSkills.get(i).initialize(i);
+        }
+    }
 }
