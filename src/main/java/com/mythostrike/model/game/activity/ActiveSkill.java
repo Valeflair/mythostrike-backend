30e385f7a77de27e4975279bae2ecfcb8444ee6b
Till <Till-Bernas@web.de>
2023-02-14 13:03:40 +0100

    fixed json recursion in champion

--- src/main/java/com/mythostrike/model/game/activity/ActiveSkill.java
+++ src/main/java/com/mythostrike/model/game/activity/ActiveSkill.java
@@ -1,14 +1,19 @@
 package com.mythostrike.model.game.activity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.mythostrike.controller.message.game.PlayerCondition;
 import com.mythostrike.model.game.activity.events.handle.PlayerHandle;
 import lombok.Getter;
 
 public abstract class ActiveSkill extends Activity {
     @Getter
+    @JsonIgnore
     protected PlayerCondition playerCondition;
     @Getter
+    @JsonIgnore
     protected PlayerHandle playerHandle;
+
+
     protected ActiveSkill(String name, String description) {
         super(name, description);
     }
