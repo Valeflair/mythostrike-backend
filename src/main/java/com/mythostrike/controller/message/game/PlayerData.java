d179f917daee42160f9501cf25e0ccbba9fb4889
Till <Till-Bernas@web.de>
2023-02-16 12:36:55 +0100

    added equipment and delayedEffects id list to PlayerData

--- src/main/java/com/mythostrike/controller/message/game/PlayerData.java
+++ src/main/java/com/mythostrike/controller/message/game/PlayerData.java
@@ -1,15 +1,22 @@
 package com.mythostrike.controller.message.game;
 
 import com.fasterxml.jackson.annotation.JsonGetter;
+import com.mythostrike.model.game.activity.Activity;
+import com.mythostrike.model.game.activity.cards.Card;
 import com.mythostrike.model.game.player.Champion;
 import com.mythostrike.model.game.player.Player;
 import com.mythostrike.model.lobby.Identity;
 
+import java.util.List;
+
 public record PlayerData(String username, int cardCount, boolean isAlive,
-                         Champion champion, int maxHp, int currentHp, Identity identity, boolean isCurrentPlayer) {
+                         Champion champion, int maxHp, int currentHp, Identity identity, boolean isCurrentPlayer,
+                         List<Integer> equipment, List<Integer> delayedEffects) {
     public PlayerData(Player player, boolean isCurrentPlayer) {
         this(player.getUsername(), player.getHandCards().size(), player.isAlive(),
-            player.getChampion(), player.getMaxHp(), player.getCurrentHp(), player.getIdentity(), isCurrentPlayer);
+            player.getChampion(), player.getMaxHp(), player.getCurrentHp(), player.getIdentity(), isCurrentPlayer,
+            player.getEquipment().getCards().stream().map(Activity::getId).toList(),
+            player.getDelayedEffect().getCards().stream().map(Activity::getId).toList());
     }
 
     @JsonGetter("identity")
