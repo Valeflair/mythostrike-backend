8322f4ba89523ae36bcdb011cba4e16e13fd3c0e
Till <Till-Bernas@web.de>
2023-02-16 20:18:32 +0100

    try bot fix, skills not working

--- src/main/java/com/mythostrike/model/game/player/Bot.java
+++ src/main/java/com/mythostrike/model/game/player/Bot.java
@@ -3,6 +3,7 @@ package com.mythostrike.model.game.player;
 import com.mythostrike.controller.GameController;
 import com.mythostrike.controller.message.game.ChampionSelectionMessage;
 import com.mythostrike.controller.message.game.HighlightMessage;
+import com.mythostrike.controller.message.game.PlayerCondition;
 import com.mythostrike.model.game.Game;
 import com.mythostrike.model.game.activity.system.PickRequest;
 import com.mythostrike.model.game.management.GameManager;
@@ -45,20 +46,62 @@ public class Bot extends Player {
             return;
         }
         //it does random shit
-        List<Integer> pickedCards = new ArrayList<>();
+        List<Integer> pickedCardIds = new ArrayList<>();
+        int pickedSkillId = -1;
+        PlayerCondition playerCondition = new PlayerCondition();
+        List<String> pickedPlayers = new ArrayList<>();
+        boolean selectedCards = false;
 
-
-        if (message.cardIds() != null && message.cardCount() != null && !message.cardCount().contains(0)) {
+        //select cards if possible
+        if (!message.cardIds().isEmpty() && !message.cardCount().isEmpty()) {
             List<Integer> shuffleCount = new ArrayList<>(message.cardCount());
             List<Integer> shuffleIds = new ArrayList<>(message.cardIds());
             Collections.shuffle(shuffleCount, Game.RANDOM_SEED);
             Collections.shuffle(shuffleIds, Game.RANDOM_SEED);
 
             for (int i = 0; i < shuffleCount.get(0); i++) {
-                pickedCards.add(shuffleIds.get(i));
+                pickedCardIds.add(shuffleIds.get(i));
+            }
+            //get player condition if only one card is selected
+            if (pickedCardIds.size() == 1) {
+                playerCondition = message.cardPlayerConditions().get(message.cardIds().indexOf(pickedCardIds.get(0)));
+            }
+            selectedCards = true;
+        //otherwise select a skill
+        } else if (!message.skillIds().isEmpty() && !message.skillCount().isEmpty()) {
+            List<Integer> shuffleCount = new ArrayList<>(message.skillCount());
+            List<Integer> shuffleIds = new ArrayList<>(message.skillIds());
+            Collections.shuffle(shuffleCount, Game.RANDOM_SEED);
+            Collections.shuffle(shuffleIds, Game.RANDOM_SEED);
+
+            //select skill and get player conditions if possible
+            if (shuffleCount.get(0) == 1) {
+                pickedSkillId = shuffleIds.get(0);
+                playerCondition = message.cardPlayerConditions().get(message.cardIds().indexOf(pickedSkillId));
             }
         }
+
+        //select players if needed
+        if ( !playerCondition.players().isEmpty() && !playerCondition.count().isEmpty()) {
+            List<Integer> shuffleCount = new ArrayList<>(playerCondition.count());
+            List<String> shufflePlayers = new ArrayList<>(playerCondition.players());
+            Collections.shuffle(shuffleCount, Game.RANDOM_SEED);
+            Collections.shuffle(shufflePlayers, Game.RANDOM_SEED);
+
+            for (int i = 0; i < shuffleCount.get(0); i++) {
+                pickedPlayers.add(shufflePlayers.get(i));
+            }
+        }
+
         gameManager.getCurrentActivity().remove(pickRequest);
-        gameManager.selectCards(getUsername(), pickedCards, new ArrayList<>());
+
+        //send selection
+        if (selectedCards) {
+            gameManager.selectCards(getUsername(), pickedCardIds, pickedPlayers);
+        } else {
+            gameManager.selectSkill(getUsername(), pickedSkillId, pickedPlayers);
+        }
+
+
     }
 }
