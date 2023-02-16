7e15fbe1d7240709fe86a8edd8367261f169a700
Till <Till-Bernas@web.de>
2023-02-16 16:05:41 +0100

    test error message

--- src/main/java/com/mythostrike/controller/RestExceptionHandler.java
+++ src/main/java/com/mythostrike/controller/RestExceptionHandler.java
@@ -3,36 +3,49 @@ package com.mythostrike.controller;
 import com.mythostrike.model.exception.IllegalInputException;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 @Slf4j
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ResponseStatusException.class)
-    public ResponseStatusException handleResponseStatusException(ResponseStatusException exception) {
-        log.debug("ResponseStatusException: '{}'", exception.getMessage());
-        throw new ResponseStatusException(exception.getStatusCode(), exception.getMessage());
+    public void handleResponseStatusException(ResponseStatusException exception) {
+        log.error("ResponseStatusException: '{}' at '{}'", exception.getReason(), exception.getStackTrace()[0]);
+        throw exception;
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseStatusException handleIllegalArgumentException(IllegalArgumentException exception) {
-        log.error("IllegalArgumentException: '{}'", exception.getMessage());
-        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, exception.getMessage());
+    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException exception,
+                                                                 WebRequest request) {
+        log.error("IllegalArgumentException: '{}' at '{}'", exception.getMessage(), exception.getStackTrace()[0]);
+
+        return handleExceptionInternal(exception, exception.getMessage(), new HttpHeaders(),
+            HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseStatusException handleIEntityNotFoundException(EntityNotFoundException exception) {
-        log.error("EntityNotFoundException: '{}'", exception.getMessage());
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, exception.getMessage());
+    protected ResponseEntity<Object> handleIEntityNotFoundException(EntityNotFoundException exception,
+                                                                    WebRequest request) {
+        log.error("EntityNotFoundException: '{}' at '{}'", exception.getMessage(), exception.getStackTrace()[0]);
+
+        return handleExceptionInternal(exception, exception.getMessage(), new HttpHeaders(),
+            HttpStatus.CONFLICT, request);
     }
 
     @ExceptionHandler(IllegalInputException.class)
-    public ResponseStatusException handleIIllegalInputException(IllegalInputException exception) {
-        log.error("IllegalInputException: '{}'", exception.getMessage());
-        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, exception.getMessage());
+    public ResponseEntity<Object> handleIIllegalInputException(IllegalInputException exception,
+                                                               WebRequest request) {
+        log.error("IllegalInputException: '{}' at '{}'", exception.getMessage(), exception.getStackTrace()[0]);
+
+        return handleExceptionInternal(exception, exception.getMessage(), new HttpHeaders(),
+            HttpStatus.BAD_REQUEST, request);
     }
 }
