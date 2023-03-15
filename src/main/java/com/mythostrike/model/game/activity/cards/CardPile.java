ac004148a2694c435d82f1d4683ef946de81ce3b
EZ Gameron <zihao.xu@student.kit.edu>
2023-03-15 23:47:05 +0100

    fixed duplicated card in pile

--- src/main/java/com/mythostrike/model/game/activity/cards/CardPile.java
+++ src/main/java/com/mythostrike/model/game/activity/cards/CardPile.java
@@ -1,8 +1,11 @@
 package com.mythostrike.model.game.activity.cards;
 
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 public class CardPile extends CardSpace {
 
@@ -14,7 +17,22 @@ public class CardPile extends CardSpace {
         super(type, type.getName(), cards);
     }
 
+
     public void add(Card card, boolean onTop) {
+        // Use a HashSet to remove duplicate of cardPile
+        Set<Integer> seenIds = new HashSet<>();
+        Iterator<Card> iter = cards.iterator();
+        while (iter.hasNext()) {
+            Card cardIn = iter.next();
+            if (seenIds.contains(cardIn.getId())) {
+                iter.remove(); // Remove duplicate card
+            } else {
+                seenIds.add(cardIn.getId()); // Add card ID to seenIds set
+            }
+        }
+        if (cards.contains(card)) {
+            return;
+        }
         if (!onTop) {
             cards.add(card);
         } else {
